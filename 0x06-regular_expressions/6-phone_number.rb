ef910f1ebbf5aaf722ac0f0931b3501fce3361bb
KimaniSarah <bibokimani@gmailcom>
2023-10-03 03:40:37 -0700

    Repetition Token #4

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -3,7 +3,7 @@ if ARGV.empty?
   puts "no input provided"
 else
   input = ARGV[0]
-  pattern = /\^d{10}$/
+  pattern = /\d{10}/
   results = input.scan(pattern)
   if results
     puts results.join
